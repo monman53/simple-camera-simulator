7fad1fb622414b2a97ca394582cd255d7466e8e4
Tetsuro Sakamoto <tetsuro53@gmail.com>
2024-06-09 19:35:48 +0900

    Advanced mode

--- src/Parameter.vue
+++ src/Parameter.vue
@@ -52,28 +52,30 @@ const nRays = computed(() => {
                 <tr>
                     <td><label><input type="checkbox" v-model="options.lensFocalPoints"> Focal points</label></td>
                 </tr>
-                <tr v-if="options.lensFocalPoints">
+                <tr v-if="options.lensFocalPoints && options.advanced">
                     <td><label><input type="checkbox" v-model="options.lensDoubleFocalPoints"> 2x Focal points</label>
                     </td>
                 </tr>
             </template>
-            <tr>
+            <tr v-if="options.advanced">
                 <td><label><input type="checkbox" v-model="options.aperture"> Aperture</label></td>
             </tr>
             <template v-if="options.lens">
-                <tr>
-                    <td><label><input type="checkbox" v-model="options.curvature"> Curvature</label></td>
-                    <td></td>
-                    <td>{{ humanReadable(lensR) }}</td>
-                </tr>
-                <tr>
-                    <td><label><input type="checkbox" v-model="options.lensIdeal"> Ideal lens</label></td>
-                </tr>
-                <tr>
-                    <td>Refractive index</td>
-                    <td><input type="range" min="1.01" max="3" step="0.001" v-model.number="lens.n"></td>
-                    <td>{{ humanReadable(lens.n) }}</td>
-                </tr>
+                <template v-if="options.advanced">
+                    <tr>
+                        <td><label><input type="checkbox" v-model="options.curvature"> Curvature</label></td>
+                        <td></td>
+                        <td>{{ humanReadable(lensR) }}</td>
+                    </tr>
+                    <tr>
+                        <td><label><input type="checkbox" v-model="options.lensIdeal"> Ideal lens</label></td>
+                    </tr>
+                    <tr>
+                        <td>Refractive index</td>
+                        <td><input type="range" min="1.01" max="3" step="0.001" v-model.number="lens.n"></td>
+                        <td>{{ humanReadable(lens.n) }}</td>
+                    </tr>
+                </template>
                 <tr>
                     <td>Focal Length</td>
                     <td></td>
@@ -84,21 +86,23 @@ const nRays = computed(() => {
                     <td></td>
                     <td>{{ humanReadable(fNumber) }}</td>
                 </tr>
-                <tr>
-                    <td>Position</td>
-                    <td></td>
-                    <td>{{ humanReadable(lens.x) }}</td>
-                </tr>
-                <tr>
-                    <td>Diameter</td>
-                    <td></td>
-                    <td>{{ humanReadable(lens.r * 2) }}</td>
-                </tr>
-                <tr>
-                    <td>Thickness</td>
-                    <td></td>
-                    <td>{{ humanReadable(lensD) }}</td>
-                </tr>
+                <template v-if="options.advanced">
+                    <tr>
+                        <td>Position</td>
+                        <td></td>
+                        <td>{{ humanReadable(lens.x) }}</td>
+                    </tr>
+                    <tr>
+                        <td>Diameter</td>
+                        <td></td>
+                        <td>{{ humanReadable(lens.r * 2) }}</td>
+                    </tr>
+                    <tr>
+                        <td>Thickness</td>
+                        <td></td>
+                        <td>{{ humanReadable(lensD) }}</td>
+                    </tr>
+                </template>
             </template>
             <!-- Screen -->
             <tr>
@@ -113,19 +117,22 @@ const nRays = computed(() => {
                 <tr v-if="options.sensorPreview">
                     <td><label><input type="checkbox" v-model="options.sensorMemory"> Memory</label></td>
                 </tr>
-                <tr>
-                    <td><label><input type="checkbox" v-model="options.circleOfConfusion"> CoC</label></td>
-                    <template v-if="options.circleOfConfusion">
-                        <td><input type="range" min="0" max="10" step="0.001" v-model.number="lens.circleOfConfusion">
-                        </td>
-                        <td>{{ humanReadable(lens.circleOfConfusion) }}</td>
-                    </template>
-                </tr>
-                <tr>
-                    <td>Diameter</td>
-                    <td></td>
-                    <td>{{ humanReadable(sensor.r * 2) }}<br></td>
-                </tr>
+                <template v-if="options.advanced">
+                    <tr>
+                        <td><label><input type="checkbox" v-model="options.circleOfConfusion"> CoC</label></td>
+                        <template v-if="options.circleOfConfusion">
+                            <td><input type="range" min="0" max="10" step="0.001"
+                                    v-model.number="lens.circleOfConfusion">
+                            </td>
+                            <td>{{ humanReadable(lens.circleOfConfusion) }}</td>
+                        </template>
+                    </tr>
+                    <tr>
+                        <td>Diameter</td>
+                        <td></td>
+                        <td>{{ humanReadable(sensor.r * 2) }}<br></td>
+                    </tr>
+                </template>
             </template>
             <!-- Other options -->
             <tr>
@@ -139,49 +146,56 @@ const nRays = computed(() => {
             <tr v-if="options.lens && options.sensor">
                 <td><label><input type="checkbox" v-model="options.angleOfView"> Guide lines</label></td>
             </tr>
-            <tr v-if="options.lens && options.sensor && options.circleOfConfusion">
-                <td><label><input type="checkbox" v-model="options.depthOfField"> Depth of field</label></td>
-            </tr>
-            <tr v-if="options.lens && options.sensor && options.circleOfConfusion && options.depthOfField">
-                <td><label><input type="checkbox" v-model="options.hyperfocalPoint"> Hyperfocal point</label></td>
-            </tr>
             <tr>
-                <td><label><input type="checkbox" v-model="options.grid"> Grid</label></td>
+                <td><label><input type="checkbox" v-model="options.advanced"> Advanced mode</label></td>
             </tr>
-            <tr>
-                <td><label><input type="checkbox" v-model="options.opticalAxis"> Optical axis</label></td>
-            </tr>
+            <template v-if="options.advanced">
+                <tr v-if="options.lens && options.sensor && options.circleOfConfusion">
+                    <td><label><input type="checkbox" v-model="options.depthOfField"> Depth of field</label></td>
+                </tr>
+                <tr v-if="options.lens && options.sensor && options.circleOfConfusion && options.depthOfField">
+                    <td><label><input type="checkbox" v-model="options.hyperfocalPoint"> Hyperfocal point</label></td>
+                </tr>
+                <tr>
+                    <td><label><input type="checkbox" v-model="options.grid"> Grid</label></td>
+                </tr>
+                <tr>
+                    <td><label><input type="checkbox" v-model="options.opticalAxis"> Optical axis</label></td>
+                </tr>
+            </template>
             <!-- Field -->
-            <tr>
-                <th colspan="3">
-                    <hr>Field
-                </th>
-            </tr>
-            <tr>
-                <td>Width</td>
-                <td></td>
-                <td>{{ humanReadable(state.width) }}</td>
-            </tr>
-            <tr>
-                <td>Height</td>
-                <td></td>
-                <td>{{ humanReadable(state.height) }}</td>
-            </tr>
-            <tr>
-                <td>Center x</td>
-                <td></td>
-                <td>{{ humanReadable(state.cx) }}</td>
-            </tr>
-            <tr>
-                <td>Center y</td>
-                <td></td>
-                <td>{{ humanReadable(state.cy) }}</td>
-            </tr>
-            <tr>
-                <td>Scale</td>
-                <td></td>
-                <td>{{ humanReadable(state.scale) }}</td>
-            </tr>
+            <template v-if="options.advanced">
+                <tr>
+                    <th colspan="3">
+                        <hr>Field
+                    </th>
+                </tr>
+                <tr>
+                    <td>Width</td>
+                    <td></td>
+                    <td>{{ humanReadable(state.width) }}</td>
+                </tr>
+                <tr>
+                    <td>Height</td>
+                    <td></td>
+                    <td>{{ humanReadable(state.height) }}</td>
+                </tr>
+                <tr>
+                    <td>Center x</td>
+                    <td></td>
+                    <td>{{ humanReadable(state.cx) }}</td>
+                </tr>
+                <tr>
+                    <td>Center y</td>
+                    <td></td>
+                    <td>{{ humanReadable(state.cy) }}</td>
+                </tr>
+                <tr>
+                    <td>Scale</td>
+                    <td></td>
+                    <td>{{ humanReadable(state.scale) }}</td>
+                </tr>
+            </template>
         </table>
     </div>
 </template>
