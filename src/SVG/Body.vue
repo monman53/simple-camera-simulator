c56e9dbd9c37c97515a097098d404051ceb375c6
monman53 <tetsuro53@gmail.com>
2024-07-05 16:03:39 +0900

    Refactor body svg

--- src/SVG/Body.vue
+++ src/SVG/Body.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import { aperture, body, lensCOGs, lensesSorted, lensFronts, lensGroups, lensRs, options, sensor } from '@/globals';
 import WithBackground from './WithBackground.vue';
-import * as h from '../handlers'
+import MoveUI from './MoveUI.vue';
+import type { Vec } from '@/math';
 
-const move = (e: any) => {
-    h.preventDefaultAndStopPropagation(e)
-    const m0 = h.getPositionOnSvg(e)
+const move = () => {
     const x0s = lensGroups.value.map(lensGroup => lensGroup.lenses.map(lens => lens.planes.map(p => p.x)))
     const sensorS0 = sensor.value.s.copy()
     const sensorT0 = sensor.value.t.copy()
     const apertureX0 = aperture.value.x
-    h.setMoveHandler((e_: any) => {
-        const d = h.getPositionDiffOnSvgApp(e_, m0)
+    return (e: any, d: Vec) => {
         lensGroups.value.forEach((lensGroup, i) => {
             lensGroup.lenses.forEach((lens, j) => {
                 lens.planes.forEach((plane, k) => {
@@ -22,13 +20,12 @@ const move = (e: any) => {
         sensor.value.s.x = sensorS0.x + d.x
         sensor.value.t.x = sensorT0.x + d.x
         aperture.value.x = apertureX0 + d.x
-    })
+    }
 }
-
 </script>
 
 <template>
-    <g @mousedown="move" class="grab">
+    <MoveUI :handler-creator="move" class="grab">
         <WithBackground :ui="true">
             <g v-if="body.front !== null && body.back !== null && body.r !== null" class="stroke-white thicker">
                 <!-- Outline -->
@@ -44,12 +41,13 @@ const move = (e: any) => {
                             <line :x1="lensCOGs[idx]" :y1="body.r" :x2="lensCOGs[idx]" :y2="lensRs[idx]" />
                         </g>
                         <g v-else>
-                            <line :x1="lensFronts[idx]" :y1="-body.r" :x2="lensFronts[idx]" :y2="-lensRs[idx]"></line>
+                            <line :x1="lensFronts[idx]" :y1="-body.r" :x2="lensFronts[idx]" :y2="-lensRs[idx]">
+                            </line>
                             <line :x1="lensFronts[idx]" :y1="body.r" :x2="lensFronts[idx]" :y2="lensRs[idx]"></line>
                         </g>
                     </g>
                 </g>
             </g>
         </WithBackground>
-    </g>
+    </MoveUI>
 </template>
